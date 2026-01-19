durr1602/UpSetPlot

commit 08f0648368122303075cbb2835ae7fea0f7f07df

fix #301

upsetplot/plotting.py
@@ -909,8 +909,8 @@ class UpSet:
             for rect in rects:
                 width = rect.get_width() + rect.get_x()
                 ax.text(
-                    width + margin,
-                    rect.get_y() + rect.get_height() * 0.5,
+                    float(np.ravel(width + margin)[0]),
+                    float(np.ravel(rect.get_y() + rect.get_height() * 0.5)[0]),
                     fmt.format(*make_args(width)),
                     ha="left",
                     va="center",
@@ -920,8 +920,8 @@ class UpSet:
             for rect in rects:
                 width = rect.get_width() + rect.get_x()
                 ax.text(
-                    width + margin,
-                    rect.get_y() + rect.get_height() * 0.5,
+                    float(np.ravel(width + margin)[0]),
+                    float(np.ravel(rect.get_y() + rect.get_height() * 0.5)[0]),
                     fmt.format(*make_args(width)),
                     ha="right",
                     va="center",
@@ -931,8 +931,8 @@ class UpSet:
             for rect in rects:
                 height = rect.get_height() + rect.get_y()
                 ax.text(
-                    rect.get_x() + rect.get_width() * 0.5,
-                    height + margin,
+                    float(np.ravel(height + margin)[0]),
+                    float(np.ravel(rect.get_x() + rect.get_width() * 0.5)[0]),
                     fmt.format(*make_args(height)),
                     ha="center",
                     va="bottom",

commit 7fa2661b7989e733bfce8a668f864084b8390afd

fix error introduced in 08f0648

upsetplot/plotting.py
@@ -931,8 +931,8 @@ class UpSet:
             for rect in rects:
                 height = rect.get_height() + rect.get_y()
                 ax.text(
-                    float(np.ravel(height + margin)[0]),
                     float(np.ravel(rect.get_x() + rect.get_width() * 0.5)[0]),
+                    float(np.ravel(height + margin)[0]),
                     fmt.format(*make_args(height)),
                     ha="center",
                     va="bottom",